8b6f106bdd98ba08aa74f376421e0918a5f5daad
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-04-24 01:32:59 +0530

    prototypes in JS

--- JAVASCRIPT/4_Classes&Objects.js
+++ JAVASCRIPT/4_Classes&Objects.js
@@ -1,1 +1,23 @@
 //! Classes & Objects
+// const student = {
+//   fullName: "Amit",
+//   marks: 98,
+//   printMarks: function () {
+//     console.log("marks : ", this.marks);
+//   },
+// };
+
+//! 1. Prototypes : objects
+const employee = {
+  calTax() {
+    console.log("tax");
+  },
+};
+
+const karanArjun = {
+  salary: 1000000,
+  yoe: 2,
+};
+
+//* calculate tax
+karanArjun.__proto__ = employee;
